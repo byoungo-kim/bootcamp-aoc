18e8179bf10886b19dab1bc70854e4b0634355fa
Byoungo Kim <bo.kim@greenlabs.co.kr>
2022-08-30 21:15:02 +0900

    update the code to solve the aoc 2018 day 1
    Signed-off-by: Byoungo Kim <bo.kim@greenlabs.co.kr>

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -2,18 +2,38 @@
   (:require [clojure.java.io :as io]))
 
 ;; Get input
-(def input (-> "aoc2018_1_1.input"
-               (io/resource)
-               (slurp)
-               (clojure.string/split-lines)))
+(defn read-input
+  "Input: a file path
+   Output: line-by-line separated array"
+  [path] (-> path 
+             (io/resource)
+             (slurp)
+             (clojure.string/split-lines)))
+
+(defn parse-int
+  "input: int-string
+  output: integer value"
+  [int-string]
+  (Integer/parseInt int-string))
+
+(defn convert-into-integer
+  "input: integer string array
+   output: int array"
+  [string-sequence]
+  (map parse-int string-sequence))
 
 ;; 파트 1
 ;; 주어진 입력의 모든 숫자를 더하시오.
 ;; 예) +10 -2 -5 +1 이 입력일 경우 4를 출력
-
+(comment
 ;; part 1
-(reduce + (for [digit input] (Integer/parseInt digit)))
+(defn sum-all [int-sequence] (reduce + int-sequence))
 
+(-> "aoc2018_1_1.input"
+    (read-input)
+    (convert-into-integer)
+    (sum-all))
+  )
 ;; ANS: 502
 
 ;; 파트 2
@@ -21,11 +41,30 @@
 ;; 예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임.
 ;; 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
 ;; atom을 쓰면 될 거 같다는 걸 찾았으나 현재는 여기까지..
-(def sumList #{})
 
-(for [number 
-      (for [digit input] (Integer/parseInt digit))]
-  (let [sum (+ number (if (empty sumList) 0 (last sumList)))]
-    (if (contains? sumList sum)
-      (prn sum)
-      (conj sumList sum))))
+(comment
+  (defn sum-until-duplicate
+    "Input: integer array
+     Output: calculate sum of array until the same sum value found"
+    [int-sequence]
+    (loop [index 0 sum 0 visited-value #{}]
+      (let [current-sum (+ sum (nth int-sequence index))]
+        (prn current-sum)
+        (if (contains? visited-value current-sum)
+          current-sum
+          (let [next-index (+ index 1)]
+            (
+             recur (if (< next-index (count int-sequence)) next-index 0) current-sum (conj visited-value current-sum)
+            )
+          )
+        )
+      )
+    )
+  )
+  (-> "aoc2018_1_1.input"
+      (read-input)
+      (convert-into-integer)
+      (sum-until-duplicate))
+  )
+
+;; ANS: 71961
