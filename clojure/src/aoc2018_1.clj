7b673984e6eeb8403ff81940859f0342aa413f26
Byoungo Kim <bo.kim@greenlabs.co.kr>
2022-08-30 17:25:51 +0900

    add work-in-progress for review
    Signed-off-by: Byoungo Kim <bo.kim@greenlabs.co.kr>

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -14,12 +14,18 @@
 ;; part 1
 (reduce + (for [digit input] (Integer/parseInt digit)))
 
+;; ANS: 502
+
 ;; 파트 2
 ;; 주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
 ;; 예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임.
 ;; 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
 ;; atom을 쓰면 될 거 같다는 걸 찾았으나 현재는 여기까지..
-;; (for [number (for [digit input] (Integer/parseInt digit))
-;;       :let [sum 0 visited {}]]
-;;   (()
-;;    (conj visited )))
+(def sumList #{})
+
+(for [number 
+      (for [digit input] (Integer/parseInt digit))]
+  (let [sum (+ number (if (empty sumList) 0 (last sumList)))]
+    (if (contains? sumList sum)
+      (prn sum)
+      (conj sumList sum))))
