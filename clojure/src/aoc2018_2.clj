fbc306437563cb1885c210f91e65ec536010a9f0
Byoungo Kim <bo.kim@greenlabs.co.kr>
2022-08-31 16:12:41 +0900

    solve day2  - solve part 1 first
    Signed-off-by: Byoungo Kim <bo.kim@greenlabs.co.kr>

--- clojure/src/aoc2018_2.clj
+++ clojure/src/aoc2018_2.clj
@@ -1,5 +1,16 @@
-(ns aoc2018-2)
+(ns aoc2018-2
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
 
+;; Get input
+(defn read-input
+  "Input: a file path
+   Output: line-by-line separated array"
+  [path] (-> path 
+             (io/resource)
+             (slurp)
+             (clojure.string/split-lines)))
+(def input-strings (read-input "aoc2018_2.input"))
 ;; 파트 1
 ;; 주어진 각각의 문자열에서, 같은 문자가 두번 혹은 세번씩 나타난다면 각각을 한번씩 센다.
 ;; 두번 나타난 문자가 있는 문자열의 수 * 세번 나타난 문자가 있는 문자열의 수를 반환하시오.
@@ -12,6 +23,74 @@
 ;; abcdee 2개의 e -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 2)
 ;; ababab 3개의 a, 3개의 b 지만 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 3)
 ;; 답 : 4 * 3 = 12
+(comment
+  (defn split-string
+    "Input: a string
+     Output: a list of letters composing a given string"
+    [string re]
+    (str/split string re))
+
+  (defn get-frequencies
+    "Input: single string
+     Output: frequencies of letters in a string"
+    [string]
+    (-> string
+        (split-string #"")
+        (frequencies)))
+
+  (defn aggregate-frequency-check
+    "Input: The frequency of a letter
+     Output: the aggregated array of duplicate check ()"
+    [frequency checked-duplicates]
+    ;; wip code: 
+    ;; map bit-or checked-duplicates [(= frequence 2) (= frequency 3)] 
+    (if (= frequency 2)
+      (map bit-or checked-duplicates [1 0])
+      (if (= frequency 3)
+        (map bit-or checked-duplicates [0 1])
+        (map bit-or checked-duplicates [0 0]))))
+  
+  (defn check-frequencies
+    "Input: frequencies of letters
+     Output: existence of letters with two and three occurence"
+    [letter-frequencies]
+    (loop [letters (keys letter-frequencies) index 0 checked-duplicates [0 0]]
+      (if (< index (count letters))
+         (recur letters (inc index) (aggregate-frequency-check (letter-frequencies (nth letters index)) checked-duplicates))
+         checked-duplicates
+      )
+    )
+  )
+  (-> "ababab"
+      (get-frequencies)
+      (check-frequencies))
+  (defn aggregate-string-check-result
+    "Input: intermediary aggregated string check results and the accumulated check result
+     Output: The newly accumulated check result"
+    [checked-duplicates num-of-duplicates]
+    (map + checked-duplicates num-of-duplicates))
+
+  (defn count-ids
+    "Input: An array of strings
+     Output: Two numbers, one is the number of ids which has the same letter twice, and the other number of whom has the same letter three times" 
+    [string-sequence]
+    (loop [index 0 num-of-duplicates [0 0]] 
+      (if (< index (count string-sequence))
+        (recur 
+         (inc index) 
+         (-> (nth string-sequence index)
+             (get-frequencies)
+             (check-frequencies)
+             (aggregate-string-check-result num-of-duplicates)))
+        num-of-duplicates))
+    )
+    
+  (count-ids ["abcdef" "bababc"])
+
+  (let [output (count-ids input-strings)]
+    (* (first output) (second output)))
+  )
+    
 
 
 ;; 파트 2
