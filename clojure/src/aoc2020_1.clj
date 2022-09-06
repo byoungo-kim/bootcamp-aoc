5c33015b4fd1a1862b1ab6f5967e4b200b6d7c64
Byoungo Kim <bo.kim@greenlabs.co.kr>
2022-09-07 08:18:02 +0900

    solve aoc2020 day1
    Signed-off-by: Byoungo Kim <bo.kim@greenlabs.co.kr>

---
+++ clojure/src/aoc2020_1.clj
@@ -0,0 +1,82 @@
+(ns aoc2020_1
+  (:require [clojure.java.io :as io]))
+
+(defn read-input
+  "parse file input into events
+   Input: file path
+   Output: (\"1721\" \"979\" \"366\" \"299\" \"675\" \"1456\")"
+  [path]
+  (->> path io/resource slurp clojure.string/split-lines))
+
+(defn pairwise-sums
+  "Returns sums of any two nums
+   Input: (\"1721\" \"979\" \"366\" \"299\" \"675\" \"1456\")
+   Output: ()"
+  [num-strings]
+  (for [i (range (count num-strings))
+        j (range (count num-strings))
+        :let [first-num (Integer/parseInt (nth num-strings i))
+              second-num (Integer/parseInt (nth num-strings j))]
+        :when (> i j)]
+    {:sum (+ first-num second-num) 
+     :first-num first-num 
+     :second-num second-num})
+  )
+
+(defn tripple-sums
+  "Returns sums of any tripple nums
+   Input: (\"1721\" \"979\" \"366\" \"299\" \"675\" \"1456\")
+   Output: ()"
+  [num-strings]
+  (for [i (range (count num-strings))
+        j (range (count num-strings))
+        k (range (count num-strings))
+        :let [first-num (Integer/parseInt (nth num-strings i))
+              second-num (Integer/parseInt (nth num-strings j))
+              third-num (Integer/parseInt (nth num-strings k))]
+        :when (and (> i j) (> j k))]
+    {:sum (+ first-num second-num third-num) 
+     :first-num first-num 
+     :second-num second-num 
+     :third-num third-num}))
+
+(defn multiplication
+  "Return the multiplication from the given input
+   Input: {:sum 2020, :first-num 299, :second-num 1721}
+   Output: 514579"
+  [input]
+  (let [first-num (:first-num (first input))
+        second-num (:second-num (first input))
+        third-num (if-not (nil? (:third-num (first input)))
+                    (:third-num (first input))
+                    1)]
+   (* first-num second-num third-num))
+  )
+
+; part-one
+(comment
+  (->> '("1721" "979" "366" "299" "675" "1456")
+       pairwise-sums
+       (drop-while #(not= 2020 (:sum %)))
+       (take 1)
+       multiplication)
+  (->> "aoc2020_1.input"
+       read-input
+       pairwise-sums
+       (drop-while #(not= 2020 (:sum %)))
+       (take 1)
+       multiplication))
+
+; part-two
+(comment
+  (->> '("1721" "979" "366" "299" "675" "1456")
+       tripple-sums
+       (drop-while #(not= 2020 (:sum %)))
+       (take 1)
+       multiplication)
+  (->> "aoc2020_1.input"
+       read-input
+       tripple-sums
+       (drop-while #(not= 2020 (:sum %)))
+       (take 1)
+       multiplication))
